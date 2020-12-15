8e1d33578093fc1a05538425cb4e0025a92bb269
Rustam Ababakirov <docker123@gmail.com>
2020-12-15 21:05:20 +0500

    Added ratings

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import Rating from "./Rating";
 
 function Product({ product }) {
-  var { _id, name, price, image, description } = product;
+  var { _id, name, price, image, description, rating, numReviews } = product;
 
   return (
     <Card className="p-3 m-3 rounded">
@@ -16,6 +17,7 @@ function Product({ product }) {
           </Card.Title>
         </a>
         <Card.Text>{description}</Card.Text>
+        <Rating rating={rating} reviews={numReviews} />
         <Button variant="primary">Buy it for ${price}</Button>
       </Card.Body>
     </Card>
